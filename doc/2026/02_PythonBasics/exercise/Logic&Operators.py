fa4ff605fe91e91b095aa06e4b510efc4eb5c230
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2026-09-09 21:45:30 -0700

    Logic & Operators

---
+++ doc/2026/02_PythonBasics/exercise/Logic&Operators.py
@@ -0,0 +1,30 @@
+# ::::: Logic & Operators ::::::
+
+# Control Flow : Control Flow Statements, Boolean Expressions :
+
+#🔸1.Control Flow Statements:if, else, elif, for, while, break, continue, and pass.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#🔸2.Boolean Expressions: True, False, bool(), any(), all(), isinstance(), ==, !=, <, >, >=, <=, and, or, not, in, not in, is, and is not.
+#bool()
+print(type(True))                           #<class 'bool'>
+print(bool(123))
+print(bool("yes"))
+print(bool(0))
+print(bool(""))
+print(bool(None))
