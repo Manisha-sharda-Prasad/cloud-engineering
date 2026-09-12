7d0e0876c3740c991258382401a7cb6f6dcd672b
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2026-09-11 18:27:42 -0700

    Logic & Operators

--- doc/2026/02_PythonBasics/exercise/ConditionalStatements.py
+++ doc/2026/02_PythonBasics/exercise/ConditionalStatements.py
@@ -1,4 +1,6 @@
 # :::::  Conditional Statements  ::::::
+import email
+from unittest import case
 
 # 🔸 Standalone If, Else, Elif, Nested If, Independent If, Inline if(Ternary), Match case
 
@@ -21,7 +23,7 @@ else:
     print("💔Better luck next time, need more practice!✍️")
 
 
-
+#Better Way
 marks = 100
 assignment = True
 if marks >= 90 and assignment is True:
@@ -37,8 +39,59 @@ print('--------Inline if(Ternary)-------')
 
 print("A" if marks >= 90 else "F")
 
-grade = "A" if marks >= 90 else "F"
+grade = "A" if marks >= 90 else "B" if marks >= 80 else "F"
 print(grade)
+
+
+
+# Match case
+print("'--------Match Case--------'")
+country = "India"
+
+if country == "United States":
+    print("US")
+elif country == "India":
+    print("IN")
+elif country == "Egypt":
+    print("EG")
+else:
+    print("Unknown Country")
+
+#Better Way
+match country:
+    case "United States" | "USA":
+        print("US")
+    case "Egypt":
+        print("EG")
+    case _:
+        print("Unknown")
+
+
+
+#: : : FUN CHALLENGE : : :
+print(":::: : : : FUN CHALLENGE : : : ::::")
+
+# Validate the quality and correctness of email values :
+# - Must not be empty| contain'.' and '@'|at-least one @ symbol| ends with'.com','.org' or '.net'|not 254 characters longer| ends with letter or digit.|
+
+#Note: Instead of Elif you can add multiple IFs,it will print all the conditions.
+email = "manny123py@.com"
+
+if email == "":
+    print("Email cannot be empty")
+elif  not (" " and "." and "@" in email):
+    print("Email must contain : '', '.','@'")
+elif email.endswith((".com " or ".org" or ".net")):
+    print("Email must contains : '.com', '.org','.net'")
+elif email.count("@") > 1:
+    print("Use only one @")
+elif len(email) > 254:
+    print("Characters are longer than 254")
+elif not email[0].isalnum() and email [-1].isalnum():
+    print("Must Start and End with Letter or Digit")
+else:
+    print("Valid Email")
+
 
 
 
