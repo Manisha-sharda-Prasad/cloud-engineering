23fe27576ee1730f90158220243358f66deb1fe3
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2026-09-16 18:07:15 -0700

    data structures

--- doc/2026/02_PythonBasics/exercise/DataStructures.py
+++ doc/2026/02_PythonBasics/exercise/DataStructures.py
@@ -1,9 +1,10 @@
+#Data Structures: Lists Fundamentals, List Advanced, Other DS
 
-# Lists, Access & Read
+#🔸Lists Fundamentals : Create Lists, Access & Read, Unpacking , Explore & Analyze, Change, Soring
 
-#🔸Create Lists[] - Lists, Nested Lists - Matrix,
+#▪️Create Lists[] - Lists, Nested Lists - Matrix:::::::::
 
-#▪️Create lists
+#Create lists
 print("------------------Create-Lists----------------------")
 empty = list()
 print(empty)
@@ -14,9 +15,7 @@ print(letters)
 numbers = list(range(5))                #printing range
 print(numbers)
 
-
-
-#▪️Nested Lists - Matrix
+#Nested Lists - Matrix
 print("--------------Nested-Lists-Matrix----------------")
 mat = [[1,2,3,4],
           [5,6,7,8]]
@@ -28,16 +27,14 @@ print(mixed_mat)
 print(type(mixed_mat))
 
 
+#▪️Access & read - indexing[], Nested Lists(Matrix), slicing():::::::::::::
 
-#🔸Access & read - indexing[], Nested Lists(Matrix), slicing(),
-
-#▪️ indexing️[]
+#indexing️[] -(Get Single item)
 print("------------------Indexing[]----------------------")
 lsts = ['a','b','c','d']
 print(lsts[0], lsts[-1])
 
-
-#▪️Nested Lists(Matrix)
+#Nested Lists(Matrix) -(Rows of [items])
 print("-----------------Nested-Lists-Matrix--------------")
 matrix = [
             ['a','b','c','d'],
@@ -48,8 +45,44 @@ print(matrix[-1])
 print(matrix[-1][-1])
 print(matrix[0][2])
 
-
-#▪️Slicing [start:end]
+#Slicing [start:end] -(Get Multiple items)
 print("-----------------Slicing-[start:end]----------------")
-print(matrix[:])    #printing all
-print(matrix[0:2])   #index [[0],[2]]
+print(matrix[:])        #printing all
+print(matrix[0:2])      #index [[0],[2]]
+print(matrix[-1][:2])   #targeting [[],[], ['i','j']]
+
+
+
+#▪️Unpacking - Unpacking Rules, lists[], Asterisk*, underscore "_"
+# Unpacking Rules
+# 1.Num of Var must match the values exactly - not less, more
+# 2.asterisk collects leftovers, fine if there are none
+
+
+#lists[]
+print("-----------------Unpacking - lists[]----------------")
+person = ["Manisha", 30, "Fashion Stylist", "USA"]
+name, age, role, country = person              #order of variables should be right
+print(name, age)
+
+#Rest Collector -Asterisk * : (first, *rest , last)
+print("--------------Rest Collector-Asterisk*-------------------")
+namee, *details, place = person
+print(namee)
+print(details)        #Collected Unnecessary stuff using '*'
+print(place)
+
+*rest, places = person      #one *asterisk at one time
+print(rest)
+print(places)
+
+# unpacking with underscore "_" : (not assigning unnecessary vars)
+print("-------------Unpacking-underscore'_'-------------------")
+info = ["Lekh", 30, "Stylist", "123", "New York"]
+user, _, ids, _, _ = info
+print(user, ids)
+
+fullname, *_ = info
+*_, state = info
+print(fullname)
+print(state)
