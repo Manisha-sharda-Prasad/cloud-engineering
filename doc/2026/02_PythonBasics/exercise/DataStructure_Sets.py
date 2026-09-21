7ced42df3520a2b45d977c696372b4c05ac65524
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2026-09-20 17:24:38 -0700

    data structures

--- doc/2026/02_PythonBasics/exercise/DataStructure_Sets.py
+++ doc/2026/02_PythonBasics/exercise/DataStructure_Sets.py
@@ -1,11 +1,12 @@
 # ⭐️ Data Structure:
-#Sets {} :-
+#Sets {} :- Sets Characteristics, Sets Fundamentals, Sets Math Methods
 
-# 🔸Sets () Characteristics: (Unordered,️No Duplicates,Not Indexed,Mutable)::::::::::::::::::::::::::::::::::::::::::::
-print("--------------------Unordered/️No Duplicate/NotIndexed/Mutable--------------------")
+
+# 🔸Sets {} Characteristics: (Unordered,️No Duplicates,Not Indexed,Mutable)::::::::::::::::::::::::::
+print("----------Unordered/️NoDuplicate/NotIndexed/Mutable---------")
 #▪️Unordered - Gives but gives right Order (stored in 'Hashtable' for 'Fast' access 'Hash f()' used):::::::
 my_sets = {120, 23, 67, 120}
-print("Stores Unordered: ", my_sets)
+print("Unordered: ", my_sets)
 
 #▪️No Duplicates - Repetition Only Unique:::::::
 print("️No Duplicates: ", my_sets)
@@ -19,3 +20,41 @@ my_sets.remove(23)
 print("Mutable: ", my_sets)
 
 
+# 🔸Sets {} Fundamentals methods::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# add,update,operators,remove,discard,pop:
+print("---------------Set-Fundamental-(methods) ----------------")
+x = {120, 23, 67, 120}
+
+x.add(50)
+x.update({1,2})
+x |= {9,11}
+
+#x.pop()                                                                 # do not use 'pop()'/ removes random item in Sets{}
+x.remove(9)
+#x.remove(None) / x.remove(100)                                          # throws 'error' if value not present
+
+x.discard(100), x.discard(None)                                          # Does not throw error if value not present
+
+print("add/update/operators/remove/discard: ", x)                        # returns {}
+
+#print(sorted(x))
+print("sorted: ", sorted(x))                                             # returns [] as used 'sorted'
+
+
+
+# 🔸Sets {} Math Methods::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#union,Operators,intersection
+# union (merging 'Unique' values from 'All Sets' into 'New Set')
+print("--------------------Set-Math-Methods --------------------")
+a = {120, 23, 67, 120, 40}
+b = {121, 24, 68, 121, 40}
+c = {122, 25, 69, 122, 40}
+
+print("Union:",a.union(b.union(c)))
+print("Operators:",a | b | c)
+print("[Union] Sorted():",sorted(a.union(b.union(c))))                      #[] 'sorted()' - optional
+
+print("Intersection value:",a.intersection(b.intersection(c)))              #intersecting/appearing/repeating value in each sets
+
+print("Difference:",a.difference(b))                                        #returns in 'a',not in 'b'
+print("Difference:",b.difference(c))                                        ##returns in 'b',not in 'c'
